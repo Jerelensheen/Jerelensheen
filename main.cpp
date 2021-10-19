5bfc81ba4804fa31fade4280ac340b9a648fb7c0
Jerelen Sheen Santacera <89186794+Jerelensheen@users.noreply.github.com>
2021-10-19 08:18:35 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,21 +1,28 @@
- 
- #include<stdio.h> 
+  
+   
+    #include <stdio.h>
 
-int main() 
-{
-    int num1; 
-    int num2 ; 
-    int num3 ; 
-    int result;
-     
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
-    printf("Enter the third number: ");
-    scanf("%d", &num3);
-    
-    //divide & multiply
-    result = num1 / num2 * num3;
-    printf("%d RPM divide %d secs multiply %d degrees is %d degrees per second", num1, num2, num3,result);
-    return 0;}
+int main(){
+    int temp;
+    printf("Please put Temperature:");
+    scanf("%d", &temp);
+    if(temp < 0){
+       printf("Its the weather is freezing!");      
+}
+  else if(temp >= 0 && temp <= 10){
+         printf("It's a very cold weather!");
+}
+  else if(temp >= 10 && temp <= 20){
+         printf("It's a cold weather!");
+}
+  else if(temp >= 20 && temp <= 30){
+         printf("It's a normal weather!");
+}
+  else if(temp >= 30 && temp <= 40){
+         printf("It's a hot weather!");
+}
+  else if(temp>=40){
+         printf("It's a very hot weather!");
+}
+  return 0;
+}
