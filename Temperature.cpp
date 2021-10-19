dd3895dd01d51d2a658324fed578334f013eea87
Jerelen Sheen Santacera <89186794+Jerelensheen@users.noreply.github.com>
2021-10-19 08:24:57 -0700

    Add files via upload

---
+++ Temperature.cpp
@@ -0,0 +1,28 @@
+  
+   
+    #include <stdio.h>
+
+int main(){
+    int temp;
+    printf("Please put Temperature:");
+    scanf("%d", &temp);
+    if(temp < 0){
+       printf("Its the weather is freezing!");      
+}
+  else if(temp >= 0 && temp <= 10){
+         printf("It's a very cold weather!");
+}
+  else if(temp >= 10 && temp <= 20){
+         printf("It's a cold weather!");
+}
+  else if(temp >= 20 && temp <= 30){
+         printf("It's a normal weather!");
+}
+  else if(temp >= 30 && temp <= 40){
+         printf("It's a hot weather!");
+}
+  else if(temp>=40){
+         printf("It's a very hot weather!");
+}
+  return 0;
+}
